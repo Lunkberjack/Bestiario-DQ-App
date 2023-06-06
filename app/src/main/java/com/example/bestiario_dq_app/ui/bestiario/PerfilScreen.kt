d3e3a88b5ddc44b12d8c149090c9060dc081f882
lucia <l.le.mi.lunk15@gmail.com>
2023-06-06 14:58:58 +0200

    Foto de perfil, nombre y descripción placeholders

--- app/src/main/java/com/example/bestiario_dq_app/ui/bestiario/PerfilScreen.kt
+++ app/src/main/java/com/example/bestiario_dq_app/ui/bestiario/PerfilScreen.kt
@@ -1,10 +1,13 @@
 package com.example.bestiario_dq_app.ui.bestiario
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,17 +16,47 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.bestiario_dq_app.R
 import com.example.bestiario_dq_app.ui.bestiario.componentes.BottomBar
 import com.example.bestiario_dq_app.ui.bestiario.componentes.BottomBarScreen
 import com.example.bestiario_dq_app.ui.bestiario.componentes.DefaultAppBar
 
+// www.artstation.com/artwork/R3vDgA
+
 @Composable
 fun PerfilScreen(navHostController: NavHostController) {
     Box(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Pantalla perfil", fontSize = 10.sp)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // El avatar (seleccionable de entre varios)
+            Image(
+                painter = painterResource(id = R.drawable.dientecillos),
+                contentDescription = "Avatar",
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(
+                        CircleShape
+                    )
+            )
+            // Nombre de usuario
+            Text(text = "Username", fontWeight = FontWeight.Black, fontSize = 30.sp)
+
+            // Descripción, estado, acciones, etc
+            Text(text = "Description, todo", fontWeight = FontWeight.Light, fontSize = 18.sp)
+        }
     }
 }
+
