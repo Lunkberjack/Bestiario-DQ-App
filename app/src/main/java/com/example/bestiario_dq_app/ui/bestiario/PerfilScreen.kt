26db6b2f125b0a415df93d23a2530be2933b1f7e
lucia <l.le.mi.lunk15@gmail.com>
2023-06-06 17:06:42 +0200

    Oh no ahora puedes cambiarte el avatar 😳

--- app/src/main/java/com/example/bestiario_dq_app/ui/bestiario/PerfilScreen.kt
+++ app/src/main/java/com/example/bestiario_dq_app/ui/bestiario/PerfilScreen.kt
@@ -1,62 +1,126 @@
 package com.example.bestiario_dq_app.ui.bestiario
 
+import android.content.res.Resources
+import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.bestiario_dq_app.R
-import com.example.bestiario_dq_app.ui.bestiario.componentes.BottomBar
-import com.example.bestiario_dq_app.ui.bestiario.componentes.BottomBarScreen
-import com.example.bestiario_dq_app.ui.bestiario.componentes.DefaultAppBar
 
-// www.artstation.com/artwork/R3vDgA
+// https://www.artstation.com/artwork/R3vDgA
 
 @Composable
 fun PerfilScreen(navHostController: NavHostController) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            // El avatar (seleccionable de entre varios)
-            Image(
-                painter = painterResource(id = R.drawable.dientecillos),
-                contentDescription = "Avatar",
-                modifier = Modifier
-                    .size(150.dp)
-                    .clip(
-                        CircleShape
-                    )
-            )
-            // Nombre de usuario
-            Text(text = "Username", fontWeight = FontWeight.Black, fontSize = 30.sp)
+    var showDialog by remember { mutableStateOf(false) }
+    var selectedImage: ImageBitmap? by remember { mutableStateOf(null) }
 
-            // Descripción, estado, acciones, etc
-            Text(text = "Description, todo", fontWeight = FontWeight.Light, fontSize = 18.sp)
-        }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Primero convierte el recurso a Bitmap usando su id, y después convierte ese Bitmap a ImageBitmap.
+        val default: ImageBitmap = BitmapFactory.decodeResource(LocalContext.current.resources, R.drawable.limo).asImageBitmap()
+        Image(
+            bitmap = selectedImage ?: default,
+            contentDescription = "Profilbild",
+            modifier = Modifier
+                .size(150.dp)
+                .clip(CircleShape)
+                .clickable { showDialog = true }
+        )
+
+        // Nombre de usuario
+        Text(text = "Username", fontWeight = FontWeight.Black, fontSize = 30.sp)
+
+        // Descripción, estado, acciones, etc
+        Text(text = "Description, todo", fontWeight = FontWeight.Light, fontSize = 18.sp)
+    }
+
+    // AlertDialog para cambiar el avatar
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(text = "Elige tu avatar") },
+            // No mostramos un texto sino nuestro selector de avatar
+            text = {
+                ImageSelector(
+                    onImageSelected = { image ->
+                        selectedImage = image
+                        showDialog = false
+                    }
+                )
+            },
+            confirmButton = {
+                TextButton(onClick = { showDialog = false }) {
+                    Text(text = "Cancelar")
+                }
+            }
+        )
     }
 }
 
+@Composable
+fun ImageSelector(onImageSelected: (ImageBitmap) -> Unit) {
+    val images = listOf(
+        R.drawable.dientecillos,
+        R.drawable.limo,
+        R.drawable.dracanino,
+        R.drawable.punkitorrinco,
+        R.drawable.almaignea,
+        R.drawable.atlas,
+        R.drawable.bultobrujo,
+        R.drawable.chumbo,
+        R.drawable.golem,
+        R.drawable.macero,
+        R.drawable.marioneta,
+        R.drawable.sanguinino,
+        R.drawable.seta
+    )
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+    ) {
+        items(images) { imageRes ->
+            val bitmap: ImageBitmap = BitmapFactory.decodeResource(LocalContext.current.resources, imageRes).asImageBitmap()
+            Image(
+                bitmap = bitmap,
+                contentDescription = "Bild",
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(bottom = 5.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .clickable { onImageSelected(bitmap) }
+            )
+        }
+    }
+}
